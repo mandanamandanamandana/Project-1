3da6a92a45f772ea577990eff352edb798808e3c
MandanaMandanaMandana <mandy7373@gmail.com>
2022-10-22 18:07:59 -0400

    new API

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const apiKey = "live_0Fn60Y8Y2D4ZP0ZJG9tXABVNC5WRW4lKeQDDvkREoaXMWb17Dldpm8Z4z4OTAHer"
+
+const baseURL = "https://api.thecatapi.com/v1/images/search"
+
 $.ajax("https://api.thecatapi.com/v1/images/search")
 .then((data) => {
     console.log(data)
-    
+    console.log(data[0].url)
 })
 
